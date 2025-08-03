721170743ce441eb2ad5274a636f2e3e2648db0f
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2025-08-03 12:53:22 -0400

    Basic flag setup

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+// Flag definitions for the CLI tool
+var path *string = flag.String("path", "", "absolute or relative file/directory path to convert")
+
+// Entry point for the program
 func main() {
-	println("Initial Commit")
+	flag.Parse()
+
+	if *path == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	info, err := os.Stat(*path)
+	if err != nil {
+		log.Fatalf("Failed to access %q: %v", *path, err)
+	}
+
+	if info.IsDir() {
+		fmt.Printf("Converting directory: %s\n", *path)
+		// TODO: Recursively comb through a directory, renaming it and all files/folders within
+	} else {
+		fmt.Printf("Converting file: %s\n", *path)
+		// TODO: Convert the single filename to snake_case
+	}
+
 }
