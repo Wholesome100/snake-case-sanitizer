e866fe9ee3db905c9698009a2d2bb19262a5a669
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2025-08-03 15:41:05 -0400

    refactor renamer and expand test cases

--- renamer/rename.go
+++ renamer/rename.go
@@ -1,11 +1,32 @@
-// Package renamer contains the logic for converting filenames to snake_case
+/*
+Package renamer contains the logic for converting filenames to snake_case.
+
+Right now the focus of the CLI tool is converting Windows-style filenames like 'My Epic Project' into
+a more cross-platform-friendly snake_case format.
+
+Converting other cases like PascalCase or camelCase to snake_case may be added in future versions.
+*/
 package renamer
 
 import (
+	"regexp"
 	"strings"
 )
 
+// Common separators in filenames
+var separators = regexp.MustCompile(`[ \-_.,\t\n]+`)
+
 // Logic for renaming individual entity names
 func ConvertName(fname string) string {
-	return strings.ToLower(strings.ReplaceAll(fname, " ", "_"))
+	words := separators.Split(fname, -1)
+
+	// Loop through our split words, ignoring empty strings, and appending valid ones after lowercasing them
+	var cleaned []string
+	for _, word := range words {
+		if word != "" {
+			cleaned = append(cleaned, strings.ToLower(word))
+		}
+	}
+
+	return strings.Join(cleaned, "_")
 }
--- renamer/rename_test.go
+++ renamer/rename_test.go
@@ -14,3 +14,58 @@ func TestReplaceWhitespace(t *testing.T) {
 		t.Errorf("Error: got %q, expected %q", msg, want)
 	}
 }
+
+func TestReplaceHyphen(t *testing.T) {
+	name := "Cool-Name"
+	want := "cool_name"
+
+	msg := ConvertName(name)
+
+	if msg != want {
+		t.Errorf("Error: got %q, expected %q", msg, want)
+	}
+}
+
+func TestReplaceUnderscore(t *testing.T) {
+	name := "Already_snake_case"
+	want := "already_snake_case"
+
+	msg := ConvertName(name)
+
+	if msg != want {
+		t.Errorf("Error: got %q, expected %q", msg, want)
+	}
+}
+
+func TestReplaceMixedSeparators(t *testing.T) {
+	name := " spaced.out,name "
+	want := "spaced_out_name"
+
+	msg := ConvertName(name)
+
+	if msg != want {
+		t.Errorf("Error: got %q, expected %q", msg, want)
+	}
+}
+
+func TestReplaceTabsAndNewlines(t *testing.T) {
+	name := "Tabs\tand\nNewlines"
+	want := "tabs_and_newlines"
+
+	msg := ConvertName(name)
+
+	if msg != want {
+		t.Errorf("Error: got %q, expected %q", msg, want)
+	}
+}
+
+func TestEmptyString(t *testing.T) {
+	name := ""
+	want := ""
+
+	msg := ConvertName(name)
+
+	if msg != want {
+		t.Errorf("Error: got %q, expected %q", msg, want)
+	}
+}
