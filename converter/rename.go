bd92ffcc4cccfe0c76e47bb55e23e4ca38522b18
Wholesome100 <77867156+Wholesome100@users.noreply.github.com>
2025-08-03 13:40:20 -0400

    implement basic testing

--- converter/rename.go
+++ converter/rename.go
@@ -1,3 +1,12 @@
+// Package converter contains the logic for converting filenames to snake_case
+// it also contains the logic for renaming directories, and for previewing changes
 package converter
 
-// TODO: Implement logic for renaming individual files
+import (
+	"strings"
+)
+
+// Logic for renaming individual entities
+func ConvertName(fname string) string {
+	return strings.ToLower(strings.ReplaceAll(fname, " ", "_"))
+}
---
+++ converter/rename_test.go
@@ -0,0 +1,16 @@
+package converter
+
+import (
+	"testing"
+)
+
+func TestReplaceWhitespace(t *testing.T) {
+	name := "My Epic File"
+	want := "my_epic_file"
+
+	msg := ConvertName(name)
+
+	if msg != want {
+		t.Errorf("Error: got %q, expected %q", msg, want)
+	}
+}
